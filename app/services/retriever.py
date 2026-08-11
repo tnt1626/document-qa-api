fc78389157e95f56aace8faa2c21f365111c64b7
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-08-11 15:38:29 +0700

    Update retriever filter by document id

--- app/services/retriever.py
+++ app/services/retriever.py
@@ -1,13 +1,24 @@
+import uuid
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.embedder import embed_text
 from app.models import Chunk
 
-async def retrieve_vec(question: str, top_k: int, db: AsyncSession):
+async def retrieve_vec(question: str, document_id: uuid.UUID, top_k: int, db: AsyncSession):
+    """
+    Retrieve document by user question
+    Args:
+        question (str): user query
+        document_id (uuid.UUID): the id of user's document
+        top_k (int): number of chunks used as document for response
+        db (AsyncSession): database session
+
+    """
     embed_vec = await embed_text(question)
     chunks = (
         await db.scalars(
             select(Chunk)
+            .where(Chunk.document_id == document_id)
             .order_by(
                 Chunk.embedding.cosine_distance(embed_vec)
             )
