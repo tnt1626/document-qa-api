c374412a91e2daec0d6d821203d45e244dcdaa17
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-08-16 13:47:47 +0700

    Update database url

--- app/database.py
+++ app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 DB_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/docqa")
+if DB_URL.startswith("postgresql://"):
+    DB_URL = DB_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(DB_URL)
 
